6a0fcb022f370795368d79470254d3b67e7b775f
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-03-12 19:40:18 +0200

    markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,26 +2,15 @@
 import { BrowserRouter, } from 'react-router-dom';
 import NavBar from './Navbar/Navbar';
 import UserRoutes from 'UserRoutes';
-// import ContactList from './ContactList/ContactList';
-// import ContactFrom from './ContactForm/ContactForm';
-// import Filter from './Filter/Filter';
 
-import css from './App.module.css';
+
+// import css from './App.module.css';
 
 export const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-08-phonebook">
       <NavBar />
       <UserRoutes />
-
-    {/* <div className={css.App}>
-      <h1 className={css.phonebook}>Phonebook</h1>
-      <ContactFrom />
-      <h2 className={css.contacts}>Contacts</h2>
-      <p className={css.text}>Find contacts by name</p>
-      <Filter/>
-      <ContactList/>
-    </div> */}
     </BrowserRouter>
   );
 };
